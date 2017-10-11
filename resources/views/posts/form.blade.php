73395b04a72fe94445a77ac41eda3d407c50bdc5
beselmonster <magorik5@gmail.com>
2017-10-11 15:53:14 +0300

    git add

---
+++ resources/views/posts/form.blade.php
@@ -0,0 +1,45 @@
+@if(count($errors)>0)
+    <div class="alert alert-danger">
+        <strong> WHOOPS</strong>There were some problems with ur input.<br><br>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+        </ul>
+
+    </div>
+
+    @endif
+
+<div class="row">
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+
+        <div class="form-group">
+
+            <strong>Title:</strong>
+            {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}
+
+        </div>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+
+        <div class="form-group">
+
+            <strong>Content:</strong>
+
+            {!! Form::textarea('content', null, array('placeholder' => 'Content','class' => 'form-control','style'=>'height:100px')) !!}
+
+        </div>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </div>
+
+</div>
